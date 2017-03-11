296a5ac3503ef48cdbad8fd8fd22f9224b692925
Vsevolod Glumov <seva17@gmail.com>
2017-03-11 23:15:19 +0300

    Updated basic abstract factory example

--- factories/abstract_factory.py
+++ factories/abstract_factory.py
@@ -1,78 +1,61 @@
-"""The simplest abstract factory example as music DAW software pseudocode"""
+"""This example is based on examples in https://en.wikipedia.org/wiki/Abstract_factory_pattern"""
 
 from random import choice
 from abc import abstractmethod
 
 
-class SampleBank(object):
-
-    def __init__(self, genre, type_):
-        self.genre = genre
-        self.type = type_
-
-    def load(self):
-        print "Loading {} {}.".format(self.genre, self.type)
-
-
-class TechnoBeatsBank(SampleBank):
-
-    def __init__(self):
-        super(TechnoBeatsBank, self).__init__("techno", "beats")
-
-
-class TechnoBassBank(SampleBank):
-    def __init__(self):
-        super(TechnoBassBank, self).__init__("techno", "bass")
-
-
-class HipHopBeatsBank(SampleBank):
-    def __init__(self):
-        super(HipHopBeatsBank, self).__init__("hip-hop", "beats")
-
-
-class HipHopBassBank(SampleBank):
-    def __init__(self):
-        super(HipHopBassBank, self).__init__("hip-hop", "bass")
-
-
-class AbstractTrackProject(object):
-    """Abstract factory class"""
+class Button(object):
+    """Abstract product"""
 
     @abstractmethod
-    def get_beats_bank(self): raise NotImplementedError()
+    def paint(self):
+        raise NotImplementedError()
+
+
+class WinButton(Button):
+    """Concrete product #1"""
+
+    def paint(self):
+        print "Render a button in a Windows style"
+
+
+class OSXButton(Button):
+    """Concrete product #2"""
+
+    def paint(self):
+        print "Render a button in an OSX style"
+
+class GUIFactory(object):
+    """Abstract factory"""
 
     @abstractmethod
-    def get_bass_bank(self): raise NotImplementedError()
-
-    def create(self, name):
-        if name == 'techno':
-            return TechnoTrackProject()
-        if name == 'hip-hop':
-            return HipHopTrackProject()
+    def create_button(self):
+        raise NotImplementedError()
 
 
-class TechnoTrackProject(AbstractTrackProject):
+class WinFactory(GUIFactory):
+    """Concrete factory #1"""
 
-    genre = 'techno'
+    def create_button(self):
+        return WinButton()
 
-    def get_beats_bank(self): return TechnoBeatsBank()
+class OSXFactory(GUIFactory):
+    """Concrete factory #2"""
 
-    def get_bass_bank(self): return TechnoBassBank()
+    def create_button(self):
+        return OSXButton()
 
+if __name__ == '__main__':
+    # TODO(gva): Not sure about this part
+    # Possibly there could be an Application obj unaware of OS that makes a call to the paint() method
+    current_os = choice(['osx', 'win', 'linux'])
+    factory = None
+    if current_os == 'win':
+        factory = WinFactory()
+    elif current_os == 'osx':
+        factory = OSXFactory()
+    else:
+        raise NotImplementedError('OS {} is not supported'.format(current_os))
 
-class HipHopTrackProject(AbstractTrackProject):
-
-    genre = 'hip-hop'
-
-    def get_beats_bank(self): return HipHopBeatsBank()
-
-    def get_bass_bank(self): return HipHopBassBank()
-
-
-if __name__ == "__main__":
-    for x in range(5):
-        project = AbstractTrackProject().create(choice(['techno', 'hip-hop']))
-        print 'User #{} wants to make some {}'.format(x+1, project.genre)
-        project.get_beats_bank().load()
-        project.get_bass_bank().load()
-        print '-'*80 if x < 4 else '- FIN -'
+    button = factory.create_button()
+    button.paint()
