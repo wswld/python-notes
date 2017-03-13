0df743683f55e564a8ee1c542e1be9120c5259ae
Vsevolod Glumov <seva17@gmail.com>
2017-03-13 23:29:31 +0300

    Added a Borg example

---
+++ singleton/borg.py
@@ -0,0 +1,19 @@
+# Not a singleton per se, but a very similar (albeit more pyhtonic) concept.
+
+
+class Borg(object):
+    __shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+
+b1 = Borg()
+b2 = Borg()
+
+b1.a = 5
+print b1.a
+print b2.a
+
+b1.a = 10
+print b1.a
+print b2.a
