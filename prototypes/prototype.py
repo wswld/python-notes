254c5e5d20d47d9461baa8861d4b4ddc0aee0541
Vsevolod Glumov <seva17@gmail.com>
2017-04-13 01:19:51 +0300

    Added a draft for prototype pattern

---
+++ prototypes/prototype.py
@@ -0,0 +1,29 @@
+from copy import deepcopy
+
+
+class Prototype(object):
+
+    def clone(self):
+        return deepcopy(self)
+
+
+class ConcretePrototype1(Prototype):
+
+    def __init__(self):
+        self.name = 'Bear'
+
+
+class ConcretePrototype2(Prototype):
+    def __init__(self):
+        self.name = 'Bull'
+
+
+c1 = ConcretePrototype1()
+c2 = ConcretePrototype2()
+c1c = c1.clone()
+c2c = c2.clone()
+
+print c1.name
+print c2.name
+print c1c.name
+print c2c.name
